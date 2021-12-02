7efac462315c4dfcc3f6ba3eedc34bc58afb4f89
addweb <sudhanshutaldar98@gmail.com>
2021-12-02 11:23:08 +0530

    added

---
+++ redux-todolist-master/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <>
+      <Todo/>
+    </>
+  );
+}
+
+export default App;
---
+++ redux-todolist-master/src/components/Todo.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './todo.css';
+import { useState } from 'react';
+import { addTodo, removeTodo, deleteTodo } from '../actions';
+import { useDispatch, useSelector } from 'react-redux';
+
+function Todo() {
+    const [inputData, setInputData] = useState('');
+    const dispatch = useDispatch();
+    const list = useSelector((state) => state.todoReducers.list);
+
+    return (
+        <>
+            <div className="main-div">
+                <div className="child-div">
+                    <figure>
+                        <figcaption>Add your list here ✌️</figcaption>
+                    </figure>
+
+                    <div className="addItems">
+                        <input type="text" placeholder="✍️ Add items .."
+                            value={inputData}
+                            onChange={(event) => setInputData(event.target.value)}
+                        />
+                        <i className="fa fa-plus add-btn" onClick={() => dispatch(addTodo(inputData), setInputData(''))}></i>
+                    </div>
+
+                    <div className="showItems">
+
+                        {
+                            list.map((elem) => {
+                                return (
+                                    <div className="eachItem" key={elem.id}>
+                                        <h3>{elem.data}</h3>
+                                        <div className="todo-btn">
+                                            <i className="far fa-trash-alt add-btn" title="Delete Item" onClick={() => dispatch(deleteTodo(elem.id), setInputData(''))}></i>
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    <div className="showItems">
+                        <button className="btn effect04" data-sm-link-text="remove All" onClick={()=>dispatch(removeTodo())}><span>Check List</span></button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Todo
